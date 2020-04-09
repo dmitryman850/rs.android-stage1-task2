fd41d1f4a9c93a5eda978218007c061078782ce1
dmitryman <dmitryman850@gmail.com>
2020-04-10 00:56:17 +0300

    add subtask2v2

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -17,14 +17,12 @@ class TimeConverter {
          when (m) {
             0 -> result = "${time[h - 1]} o' clock"
             1 -> result = "${time[m - 1]} minute past ${time[h - 1]}"
-            in 2..14 -> result = "${time[m - 1]} minutes past ${time[h - 1]}"
+            in 2..14, in 16..29 -> result = "${time[m - 1]} minutes past ${time[h - 1]}"
             15 -> result = "quarter past ${time[h - 1]}"
-            in 16..29 -> result = "${time[m - 1]} minutes past ${time[h - 1]}"
             30 -> result = "half past ${time[h - 1]}"
             31 -> result = "${time[m - 1]} minute to ${time[h - 1]}"
-            in 32..44 -> result = "${time[59 - m]} minutes to ${time[h]}"
+            in 32..44, in 46..59 -> result = "${time[59 - m]} minutes to ${time[h]}"
             45 -> result = "quarter to ${time[h]}"
-            in 46..59 -> result = "${time[59 - m]} minutes to ${time[h]}"
         }
         println(result)
         return result
