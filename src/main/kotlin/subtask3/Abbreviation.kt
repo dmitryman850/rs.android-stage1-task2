ef64cde11f8ca48f3fb94739c49f89faf9bb9d16
dmitryman <dmitryman850@gmail.com>
2020-04-10 19:21:33 +0300

    add subtask3

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -1,9 +1,31 @@
 package subtask3
 
+import kotlin.system.exitProcess
+
 class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        val AA = a.toUpperCase()
+        val result = mutableListOf<String>()
+        for(char1 in AA){
+            for(char2 in b){
+                if (char1 == char2) {
+                    result.add(char1.toString())
+                }
+            }
+        }
+        println(result)
+        val res = mutableListOf<String>()
+        for (char in b) {
+            res.add(char.toString())
+        }
+        println(res)
+        if (result == res) {
+            return "YES"
+        } else {
+            return "NO"
+        }
+        //throw NotImplementedError("Not implemented")
     }
 }
